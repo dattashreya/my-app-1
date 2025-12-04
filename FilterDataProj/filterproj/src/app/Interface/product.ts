d3ced2b32ae4698e6260254a401b1f524f53ed3b
Shreya Datta <dattashreya1998@gmail.com>
2025-12-04 22:28:54 +0530

    Cart Demo

---
+++ FilterDataProj/filterproj/src/app/Interface/product.ts
@@ -0,0 +1,8 @@
+interface Product {
+  id?: number;
+  name?: string;
+  description?: string;
+  price?: number;
+  imageUrl?: string;
+  // Add other product properties as needed
+}
--- FilterDataProj/filterproj/src/app/app.component.ts
+++ FilterDataProj/filterproj/src/app/app.component.ts
@@ -9,32 +9,55 @@ import { DataService } from './Service/data.service';
 })
 export class AppComponent {
   title = 'filterproj';
-
+  titleFlagOff: boolean = false;
   items: Item[] = [];
-
   constructor(private dService: DataService) { }
-
   filteredItems: Item[] = [...this.items]; // Initialize with all items
   filterText: string = ''; // Input for filtering
-
   ngOnInit(): void {
     this.dService.getPosts().subscribe(
       (data) => {
         this.items = data;
-        console.log('Data from api....',this.items);
+        // console.log('Data from api....',this.items);
       });
   }
 
   onFilterChange(): void {
     const searchText = this.filterText.toLowerCase();
     this.filteredItems = this.items;
-    console.log('Filter to do.......',this.filteredItems);
+    // console.log('Filter to do.......',this.filteredItems);
     
     this.filteredItems =  this.filteredItems.filter(item =>
       item.name.toLowerCase().includes(searchText) ||
       item.id.toString().includes(searchText)
       // item.username.toLowerCase().includes(searchText)
     );
+  }
+
+  //  To do: new project to create
+
+  // productList = [
+  //  {name: 'Z900', price: 8799},
+  //  {name: 'shubert helmet', price: 999},
+  //  {name: 'sport gloves', price: 99}
+  // ];
+
+  // productList: Product[] = [];
+ cartProductList = [];
+
+
+
+ addProductToCart(product: any)
+  {
+    const productExistInCart = this.cartProductList.find(({name}) => name === product.name); // find product by name
+    if (!productExistInCart) {
+      this.cartProductList.push({...product, num:1}); // enhance "porduct" opject with "num" property
+      return;
+    }
+    productExistInCart.num += 1;
+ }
+  removeProduct(product: any) {
+   this.cartProductList = this.cartProductList.filter(({name}) => name !== product.name)
   }
 
 }
